10ac8b7592c7e4a0859a10557e06957740c83c74
lucciii33 <angelomaiele@gmail.com>
2022-06-10 20:36:27 +0000

    token working

--- src/api/routes.py
+++ src/api/routes.py
@@ -18,7 +18,7 @@ api = Blueprint('api', __name__)
 def create_token():
     email = request.json.get("email", None)
     password = request.json.get("password", None)
-    if email != "test" or password != "test":
+    if email != "pet" or password != "pet":
         # the user was not found on the database
         return jsonify({"msg": "Bad username or password"}), 401
     access_token = create_access_token(identity=email)
--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -1,59 +1,81 @@
 const getState = ({ getStore, getActions, setStore }) => {
-	return {
-		store: {
-			profile: []
-		},
-		actions: {
-			// Use getActions to call a function within a fuction
+  return {
+    store: {
+      profile: [],
+    },
+    actions: {
+      // Use getActions to call a function within a fuction
 
-			// createProfile: async (title, aboutme, howicanhelp, services, certifications, comments) => {
-			// 	try {
-			// 		const edit = await fetch(process.env.BACKEND_URL + `/api/profile`,
+      // createProfile: async (title, aboutme, howicanhelp, services, certifications, comments) => {
+      // 	try {
+      // 		const edit = await fetch(process.env.BACKEND_URL + `/api/profile`,
 
-			// 			{
-			// 				method: "POST",
-			// 				headers: { "Content-Type": "application/json" },
-			// 				body: JSON.stringify({
-			// 					title, aboutme, howicanhelp, services, certifications, comments
-			// 				}),
-			// 			}
-			// 		)
-			// 		if (edit.ok) {
-			// 			const resp = await edit.json()
-			// 			localStorage.setItem('profile', JSON.stringify(resp))
-			// 			setStore({ profile: resp })
+      // 			{
+      // 				method: "POST",
+      // 				headers: { "Content-Type": "application/json" },
+      // 				body: JSON.stringify({
+      // 					title, aboutme, howicanhelp, services, certifications, comments
+      // 				}),
+      // 			}
+      // 		)
+      // 		if (edit.ok) {
+      // 			const resp = await edit.json()
+      // 			localStorage.setItem('profile', JSON.stringify(resp))
+      // 			setStore({ profile: resp })
 
-			// 		}
-			// 	} catch (error) {
-			// 		throw Error("something went wrong")
-			// 	}
-			// },
-			addprofile: (aboutme, howicanhelp, services, certifications) => {
-				fetch(process.env.BACKEND_URL + `/api/profile`, {
-					method: "POST",
-					headers: { "Content-Type": "application/json" },
-					body: JSON.stringify({
-						aboutme,
-						howicanhelp,
-						services,
-						certifications,
-					})
-				})
-					.then(res => res.json())
-					.then(() => {
-						getActions().getProfileData();
-					})
-					.catch(err => console.log(err));
-			},
+      // 		}
+      // 	} catch (error) {
+      // 		throw Error("something went wrong")
+      // 	}
+      // },
+      addprofile: (aboutme, howicanhelp, services, certifications) => {
+        fetch(process.env.BACKEND_URL + `/api/profile`, {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({
+            aboutme,
+            howicanhelp,
+            services,
+            certifications,
+          }),
+        })
+          .then((res) => res.json())
+          .then(() => {
+            getActions().getProfileData();
+          })
+          .catch((err) => console.log(err));
+      },
 
-			getProfileData: () => {
-				fetch(process.env.BACKEND_URL + `/api/profile`)
-					.then(res => res.json())
-					.then(dataJson => setStore({ profile: dataJson }))
-					.catch(err => console.log(err));
-			},
-		}
-	};
+      getProfileData: () => {
+        fetch(process.env.BACKEND_URL + `/api/profile`)
+          .then((res) => res.json())
+          .then((dataJson) => setStore({ profile: dataJson }))
+          .catch((err) => console.log(err));
+      },
+
+      /////////////here is the token
+      loginToken: (email, password) => {
+        const opts = {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({ email, password }),
+        };
+        fetch(`https://3001-lucciii33-petapps-lgih9tai7yq.ws-us47.gitpod.io/api/token`, opts)
+          .then((resp) => {
+            if (resp.status === 200) {
+              return resp.json();
+            } else {
+              alert("some errors");
+            }
+          })
+
+          .then()
+          .catch((error) => {
+            console.error("there was an error!!!", error);
+          });
+      },
+    },
+  };
 };
 
 export default getState;
